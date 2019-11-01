156d19e21895bc6ea34f23fb16b03a8f323885a5
Mrordiggian <adamovich-dmitrii@mail.ru>
2019-11-01 11:39:51 +0300

    Updates

---
+++ precache-manifest.82d8ae9f5520f5c3afdff3ad40ba5b29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "324058898088ca3bb0273b31848def0a",
+    "url": "/key-group/index.html"
+  },
+  {
+    "revision": "59e7cfc06acc743a7ca9",
+    "url": "/key-group/static/css/main.cbdfd4c9.chunk.css"
+  },
+  {
+    "revision": "df07caab911b6d6536df",
+    "url": "/key-group/static/js/2.37619161.chunk.js"
+  },
+  {
+    "revision": "59e7cfc06acc743a7ca9",
+    "url": "/key-group/static/js/main.14024181.chunk.js"
+  },
+  {
+    "revision": "516c6dac21326c8e4893",
+    "url": "/key-group/static/js/runtime-main.c17a423b.js"
+  }
+]);
